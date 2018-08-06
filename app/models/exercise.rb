452bd2ed5a017e9649b01f5aa7944ae633808841
github username <github email address>
2018-08-06 13:17:56 -0400

    submit category id for each exercise

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,4 +3,11 @@ class Exercise < ApplicationRecord
   belongs_to :category
   has_many :exercise_workouts
   has_many :workouts, through: :exercise_workouts
+  validates :name, presence: true
+  validates :sets, presence: true
+  validates :reps, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+
+  scope :specific_category, ->(category_id) { where("category_id = ?", category_id) }
 end
