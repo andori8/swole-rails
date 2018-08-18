94c8a7bcbbe5a1c65fc3c17335a575b57c59ade8
andori8 <github email address>
2018-08-17 21:56:12 -0400

    spec checklist

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if current_user.name.blank? || current_user.username.blank?
-      edit_user_registration_path
+      redirect_to edit_user_registration_path
     else
       redirect_to workouts_path
     end
