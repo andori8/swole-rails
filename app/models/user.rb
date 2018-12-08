594c1046b960b3d7edde700eedd4bb78d72baad6
Ryan <falcsmike7@gmail.com>
2018-12-08 09:17:50 -0500

    added seed files

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :username, presence: true, unless: -> { from_omniauth? }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(id: 1, name: "Ryan", username:"andori", email:"andori@gmail.com", password:"admin", password_confirmation:"admin")
+User.create(id: 2, name: "test", username:"test", email:"test@example.com", password: "password", password_confirmation: "password")
+User.create(id: 3, name: "try", username:"try", email:"try@example.com", password: "password", password_confirmation: "password")
+
+Workout.create(title: "Chest Pump", time: "30 minutes", user_id: 1)
+Workout.create(title: "Leg Pump", time: "60 minutes", user_id: 2)
+Workout.create(title: "Easy Mode", time: "10 minutes", user_id: 3)
+
+Exercise.create(name:"Bench Press", sets: 5 , reps: 10, description:"great exercise", user_id: 1, category_id: 1)
+Exercise.create(name:"Squat", sets: 3, reps: 12, description:"get killer legs", user_id: 2, category_id: 5)
+Exercise.create(name:"Push Ups", sets: 5, reps: 12, description:"everybody should do these", user_id: 3, category_id: 1)
+
 
 Category.create(name: "Chest")
 Category.create(name: "Tris")
