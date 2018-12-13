8249d80d4f182f941456e6d1bc65d06a48c2a78a
Ryan <falcsmike7@gmail.com>
2018-12-12 22:51:54 -0500

    make submit button clickable more than once, configured in config folder

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.action_view.automatically_disable_submit_tag = false
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
